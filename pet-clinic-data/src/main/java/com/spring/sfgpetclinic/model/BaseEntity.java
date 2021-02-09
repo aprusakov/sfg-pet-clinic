2c1744902ed48cdb6e3e379ea1c839e943b91af2
Alex <>
2021-02-09 14:05:48 +0700

    Adding BaseEntity

---
+++ pet-clinic-data/src/main/java/com/spring/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.spring.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable
+{
+    private Long id;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(final Long id)
+    {
+        this.id = id;
+    }
+}
